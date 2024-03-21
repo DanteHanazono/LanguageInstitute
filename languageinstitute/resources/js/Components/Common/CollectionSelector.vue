b31ec06465d4bb18a6fad00b5467679e4399b3d8
Rhady Lopez <rslopez0611@gmail.com>
2024-03-21 18:42:02 -0500

    Fixed CollectionSelector

--- languageinstitute/resources/js/Components/Common/CollectionSelector.vue
+++ languageinstitute/resources/js/Components/Common/CollectionSelector.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
 
 defineProps({
     collection: {
@@ -10,19 +11,49 @@ defineProps({
 
 const currentSelection = ref(1);
 const selection = ref([]);
+const emit = defineEmits(['onCategories'])
 const handleAddToSelection = () => {
-    selection.value.push(currentSelection);
+    let alreadyExists = false
+    selection.value.forEach(item => {
+        if (item.id === currentSelection.value.id) {
+            return alreadyExists = true;
+        }
+    })
+    if (alreadyExists) {
+        return
+    }
+    selection.value.push(currentSelection.value);
+    emit('onCategories', selection.value)
+}
+
+const handleRemoveSelection = (index) => {
+    selection.value = selection.value.filter(item => item.id !== index);
+    emit('onCategories', selection.value)
 }
 </script>
 
 <template>
-    <select v-model="currentSelection">
-        <option v-for="(item, index) in collection" :key="index" :value="item">{{ item?.name }}</option>
-    </select>
-    <button @click="handleAddToSelection">Add</button>
-    <div>
-        <ul>
-            <li v-for="(item, index) in selection" :key="index">{{ item }}</li>
-        </ul>
+    <div class="w-full">
+        <div class="flex"> <select v-model="currentSelection"
+                class="mt-1.5 w-medium rounded-lg border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 sm:text-sm">
+                <option v-for="(item, index) in collection" :key="index" :value="item">{{ item?.name }}</option>
+            </select>
+            <SecondaryButton class="mt-1.5 mr-2 ml-2" type="button" @click="handleAddToSelection">Add</SecondaryButton>
+        </div>
+        <div>
+            <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+                <li class="flex justify-between py-2 cursor-pointer gap-x-6" v-for="item in selection"
+                    @click="handleRemoveSelection(item.id)">
+                    <div class="flex min-w-0 gap-x-4">
+                        <div class="flex-auto min-w-0">
+                            <p class="text-sm leading-6 text-gray-800 dark:text-white">
+                                {{ item.name }}
+                                <span class="float-right mt-0.5 ml-2 mr-2 font-semibold text-red-600">X</span>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
--- languageinstitute/resources/js/Components/Lessons/Form.vue
+++ languageinstitute/resources/js/Components/Lessons/Form.vue
@@ -1,4 +1,9 @@
 <script>
+export default {
+    name: 'LessonsForm'
+}
+</script>
+<script setup>
 import FormSection from '@/Components/FormSection.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import InputError from '@/Components/InputError.vue';
@@ -6,38 +11,33 @@ import InputLabel from '@/Components/InputLabel.vue';
 import TextInput from '@/Components/TextInput.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 import CollectionSelector from '@/Components/Common/CollectionSelector.vue';
+import { ref } from 'vue';
 
-export default {
-    name: 'LessonsForm',
-    components: {
-        FormSection,
-        PrimaryButton,
-        InputError,
-        InputLabel,
-        TextInput,
-        SecondaryButton,
+defineProps({
+    form: {
+        type: Object,
+        required: true,
     },
-    props: {
-        form: {
-            type: Object,
-            required: true,
-        },
-        updating: {
-            type: Boolean,
-            required: false,
-            default: false,
-        },
-        categories: {
-            type: Object,
-            required: true
-        },
-        levels: {
-            type: Object,
-            required: true
-        }
+    updating: {
+        type: Boolean,
+        required: false,
+        default: false,
     },
-    emits: ['submit'],
-};
+    categories: {
+        type: Object,
+        required: true
+    },
+    levels: {
+        type: Object,
+        required: true
+    },
+})
+
+const categorieSelected = ref([])
+const onCategories = (_categories) => {
+    categorieSelected.value = _categories;
+}
+defineEmits(['submit']);
 </script>
 <template>
     <FormSection @submitted="$emit('submit')">
@@ -59,24 +59,31 @@ export default {
                 <InputError :message="$page.props.errors.description" class="mt-2" />
 
                 <InputLabel class="mt-2" for="content_url" value="Content Url" />
-                <TextInput id="content_url" v-model="form.conten_url" type="text" autocomplete="content_url"
+                <TextInput id="content_url" v-model="form.content_url" type="text" autocomplete="content_url"
                     class="block w-full mt-1" />
                 <InputError :message="$page.props.errors.content_url" class="mt-2" />
 
                 <InputLabel class="mt-2" for="pdf_url" value="PDF File" />
                 <SecondaryButton class="mt-2 mr-2" type="button" @click="">Upload PDF </SecondaryButton>
                 <InputError :message="$page.props.errors.pdf_url" class="mt-2" />
-
-                <InputLabel class="mt-2" for="level_id" value="Select Level" />
-                <select
-                    class="mt-1.5 w-small rounded-lg border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 sm:text-sm"
-                    name="level_id" id="level_id">
-                    <option v-for="level in levels" value="level.id">{{ level.name }}</option>
-                </select>
-                <InputError :message="$page.props.errors.level_id" class="mt-2" />
-
-                <InputLabel class="mt-2" for="categories" value="Select Categories" />
-                <CollectionSelector :collection="categories" />
+                <div class="w-full mt-2">
+                    <div class="flex">
+                        <div class="w-1/2">
+                            <InputLabel for="level_id" value="Select Level" />
+                            <select
+                                class="mt-1.5 w-small rounded-lg border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 sm:text-sm"
+                                name="level_id" id="level_id">
+                                <option v-for="level in levels" value="level.id">{{ level.name }}</option>
+                            </select>
+                            <InputError :message="$page.props.errors.level_id" class="mt-2" />
+                        </div>
+                        <div class="w-1/2">
+                            <InputLabel for="categories" value="Select Categories" />
+                            <CollectionSelector name="categories" id="categories" :collection="categories"
+                                @onCategories="onCategories" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </template>
         <template #actions>
